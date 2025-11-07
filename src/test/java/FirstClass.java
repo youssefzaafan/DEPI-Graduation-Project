a20ead068122f03dd866e614708e1fea34ace52c
youssefzaafan <youssefzafan@gmail.com>
2025-11-07 17:11:59 +0200

    Initial commit

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.webdriver.OptionsWithArguments;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FirstClass {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+
+
+
+       /* driver.findElement(By.linkText("Form Authentication")).click();
+        driver.manage().window().maximize();
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        
+        System.out.println(driver.getTitle());
+
+        driver.navigate().back();
+        driver.findElement(By.linkText("A/B Testing")).click();
+        Thread.sleep(2000);
+        driver.navigate().back();
+
+        Thread.sleep(5000);
+        driver.quit();*/
+    }
+}
