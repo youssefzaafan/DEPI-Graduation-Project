40937c89e79d1e9a8cd31690b6c4f32356e90f23
marianashawky <76921092+marianashawky@users.noreply.github.com>
2025-11-08 17:45:43 +0200

    edit

--- src/test/java/org/example/utils/BaseTest.java
+++ src/test/java/org/example/utils/BaseTest.java
@@ -9,13 +9,14 @@ import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected final String BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.get(BASE_URL);
     }
 
     @AfterMethod
